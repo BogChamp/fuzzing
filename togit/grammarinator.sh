c34a0c143e8c1acf3ea7f6762f7a7b9a404d19c3
bogdan <bogdan_aleksandrov_2014@mail.ru>
2022-05-26 15:14:46 +0300

    fixed

--- togit/grammarinator.sh
+++ togit/grammarinator.sh
@@ -1,1 +1,2 @@
 grammarinator-generate -l smartygram/SUnlexer.py -p smartygram/SUnparser.py -d 50 -o tpls/test_%d.tpl -n 30
+mv tpls/* templates/
--- togit/script.sh
+++ togit/script.sh
@@ -1,2 +1,2 @@
 java -Xss1g -Xms256m -Xmx4g -jar ./tribble.jar generate --suffix=.tpl --mode=50-50-probabilistic-20 --out-dir=tpls --grammar-file=smarty7.bnf
-mv tpls/* smarty/vendor/smarty/smarty/templates/
+mv tpls/* templates/
